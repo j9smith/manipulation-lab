c12548c9c677738993270213e98cc768aa7de329
j9smith <j.smith9103@outlook.com>
2025-07-09 18:38:26 +0000

    Separated controller logic from teleop class

---
+++ source/manipulation_lab/scripts/utils/controller_interface.py
@@ -0,0 +1,79 @@
+import logging
+logger = logging.getLogger("ManipulationLab.ControllerInterface")
+
+from multiprocessing import Array
+import torch
+
+class ControllerInterface:
+    def __init__(self, remote_connection=False, port=8888, controller: str="xbox", 
+                 expected_action_dims: int=7, expected_command_dims: int=4, **kwargs):
+
+        self.controller = controller
+
+        # Remote operation config
+        self.remote_connection = remote_connection
+        self.port = port
+        self.controller_array: Array = None
+
+        # Expected dimensions of the controller array
+        self.expected_action_dims = expected_action_dims
+        self.expected_command_dims = expected_command_dims
+
+        if remote_connection:
+            self._initialise_socket_connection()
+
+        self._initialise_controller()
+
+    def _initialise_controller(self):
+        """
+        Print control commands.
+        """
+        if self.controller == "xbox":
+            logger.info("Xbox Controller: A = start, B = abort, X = finish, Y = pause")
+
+    def _initialise_socket_connection(self):
+        """
+        Initialise a socket connection to accept teleoperation from remote server
+        """
+        from manipulation_lab.scripts.utils.socket_listener import start_socket_listener
+        logger.info(f"Initialising remote connection")
+        expected_dims = self.expected_action_dims + self.expected_command_dims
+        # Initialise a shared array between the main process and the socket listener (threaded)
+        self.controller_array = Array('f', [0.0] * expected_dims)
+        self.thread = start_socket_listener(
+            self.controller_array, 
+            port=self.port, 
+            expected_dims=expected_dims
+        )
+
+    def get_action(self):
+        if self.remote_connection and self.controller_array is not None:
+            action = list(self.controller_array)[:self.expected_action_dims]
+        else: 
+            action = [0.0] * self.expected_action_dims
+
+        return torch.tensor(action, dtype=torch.float32)
+
+    def get_episode_commands(self):
+        if self.remote_connection and self.controller_array is not None:
+            episode_commands = list(self.controller_array)[self.expected_action_dims:]
+        else:
+            episode_commands = [0.0] * self.expected_command_dims
+
+        if episode_commands[0] == 1: # A
+            return "start"
+        elif episode_commands[1] == 1: # B
+            return "abort"
+        elif episode_commands[2] == 1: # X
+            return "finish"
+        elif episode_commands[3] == 1: # Y
+            return "pause"
+        else:
+            return None
+
+    def _get_xbox_input(self):
+        """
+        Get input from an Xbox controller.
+        """
+        if self.remote_connection and self.controller_array is not None:
+            pass
--- source/manipulation_lab/scripts/utils/teleop_handler.py
+++ source/manipulation_lab/scripts/utils/teleop_handler.py
@@ -4,67 +4,88 @@ import logging
 logger = logging.getLogger("ManipulationLab.TeleopHandler")
 
 from manipulation_lab.scripts.utils.action_handler import ActionHandler
-from multiprocessing import Array
-import torch
+from manipulation_lab.scripts.utils.obs_handler import ObservationHandler
+from manipulation_lab.scripts.utils.dataset_writer import DatasetWriter
+from manipulation_lab.scripts.utils.controller_interface import ControllerInterface
+
+from isaaclab.scene import InteractiveScene
+
+import time
 
 class TeleopHandler:
-    def __init__(self, env, remote_connection=True):
+    def __init__(self, env, cfg):
         self.env = env
         self.sim = self.env.unwrapped.sim
         self.scene = self.env.unwrapped.scene
+        self.sim_steps = 0
+        self.cfg = cfg
+
+        self.controller = ControllerInterface(**cfg.controller)
         self.action_handler = ActionHandler(env=self.env, control_mode="delta_cartesian")
-        self.remote_connection = remote_connection
-        self.action_array: Array = None
-        
-        if remote_connection:
-            self._initialise_socket_connection()
+        self.obs_handler = ObservationHandler(env=self.env)
+        self.dataset_writer = DatasetWriter(
+            env_name=self.env.env_name,
+            task_name=self.env.task_name,
+            sim_dt=self.sim.get_physics_dt(),
+            **cfg.dataset_writer
+        )
 
-    def _initialise_socket_connection(self):
-        """
-        Initialise a socket connection to accept teleoperation from remote server
-        """
-        from manipulation_lab.scripts.utils.socket_listener import start_socket_listener
-        logger.info(f"Initialising remote connection")
-
-        # Initialise a shared array between the main process and the socket listener (threaded)
-        self.action_array = Array('f', [0.0] * 7)
-        self.thread = start_socket_listener(self.action_array)
-
-    def _get_action(self):
-        if self.remote_connection and self.action_array is not None:
-            action = list(self.action_array)
-        else: 
-            action = [0.0] * 7
-
-        return torch.tensor(action, dtype=torch.float32)
+        self.target_fps = cfg.dataset_writer.target_fps
 
     def run_teleop(self, simulation_app):
-        self.action_handler = ActionHandler(env=self.env, control_mode="delta_cartesian")
 
         sim_dt = self.sim.get_physics_dt()
 
-        target_fps = 30
+        self.target_fps = 30
 
         # Compute number of sim steps before capturing observations
         # Round to account for floating point precision errors in sim_dt
-        capture_frequency = round(1.0 / target_fps / sim_dt)
+        capture_frequency = round(1.0 / self.target_fps / sim_dt)
 
-        logger.info(f"Recording at {target_fps} FPS, which is every {capture_frequency} sim steps. (sim_dt={sim_dt:.4f}s)")
-
-        sim_steps = 0
+        logger.info(f"Recording at {self.target_fps} FPS, which is every {capture_frequency} sim steps. (sim_dt={sim_dt:.4f}s)")
 
         while simulation_app.is_running():
-            sim_steps += 1
+            episode_command = self.controller.get_episode_commands()
+            if episode_command is None and self.dataset_writer.episode_started:
+                _continue_episode()
+            else:
+                if episode_command == "start":
+                    self.dataset_writer.start_episode()
+                    _continue_episode()
+                elif episode_command == "pause":
+                    self.dataset_writer.pause_episode()
+                elif episode_command == "abort":
+                    self.dataset_writer.abort_episode()
+                    _reset_scene()
+                elif episode_command == "finish":
+                    self.dataset_writer.end_episode()
+                    _reset_scene()
 
-            # Record observations at target FPS
-            if sim_steps % capture_frequency == 0:
+            def _reset_scene():
+                # TODO: Write scene reset logic
                 pass
 
-            # Get the teleoperation action
-            action = self._get_action()
+            def _continue_episode():
+                self.sim_steps += 1
 
-            # Apply the teleoperation action to the robot
-            self.action_handler.apply(action=action)
+                # Get the teleoperated action
+                action = self.controller.get_action()
+
+                # Record observations at target FPS
+                if self.sim_steps % capture_frequency == 0:
+                    is_first = (self.sim_steps == 0)
+                    is_last = False # TODO: Add _get_dones to task scene, then put it here (returns bool)
+                    obs = self.obs_handler.get_obs()
+                    self.dataset_writer.append_frame(
+                        obs=obs,
+                        action=action,
+                        is_first=is_first,
+                        is_last=is_last,
+                        sim_steps=self.sim_steps
+                    )
+
+                # Apply the teleoperated action to the robot
+                self.action_handler.apply(action=action)
 
             # Step the simulation
             self.sim.step()
