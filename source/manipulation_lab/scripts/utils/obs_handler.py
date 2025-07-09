25d5491bc513588f082cc515379c606192bf2360
j9smith <j.smith9103@outlook.com>
2025-07-09 18:40:56 +0000

    Added observation logic

--- source/manipulation_lab/scripts/utils/obs_handler.py
+++ source/manipulation_lab/scripts/utils/obs_handler.py
@@ -1,8 +1,37 @@
 """Handles observations from the environment."""
 
 class ObservationHandler:
-    def get_obs(self, env):
-        obs = {}
+    def __init__(self, env):
+        self.env = env
+        self.scene = self.env.unwrapped.scene
+        self.robot = self.scene.articulations["robot"]
+        self.sensors = self.scene.sensors
 
-        for sensor_name, sensor_obj in env.unwrapped.scene.sensors.items():
-            sensor_data = sensor_obj.data
+    def get_obs(self):
+        return {
+            "sensors": self._get_sensor_obs(),
+            "robot": self._get_robot_obs()
+        }
+
+    def _get_sensor_obs(self):
+        sensor_obs = {}
+
+        # Iterate over all sensors in scene and get their output
+        for sensor_name, sensor_obj in self.sensors.items():
+            # sensor_obj.data.output is a dictionary of type:tensor, e.g., {'rgb': tensor, 'depth': tensor}
+            # Move tensors to CPU then convert to numpy for portability
+            processed_output = {type: tensor.cpu().numpy() for type, tensor in sensor_obj.data.output.items()}
+            sensor_obs[sensor_name] = processed_output
+
+        # sensor_obs is dict: {sensor_name: {type:ndarray}}}
+        return sensor_obs
+
+    def _get_robot_obs(self):
+        joint_pos = self.robot.data.joint_pos.cpu().numpy()
+        joint_vel = self.robot.data.joint_vel.cpu().numpy()
+
+        robot_obs = {
+            "joint_pos": joint_pos,
+            "joint_vel": joint_vel
+        }
+        return robot_obs
