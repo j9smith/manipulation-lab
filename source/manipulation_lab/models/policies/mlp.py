7728614e1fa41d964d430037cac1198eb5d9bcdf
j9smith <j.smith9103@outlook.com>
2025-07-11 17:38:20 +0000

    Added a basic MLP for testing training and inference

---
+++ source/manipulation_lab/models/policies/mlp.py
@@ -0,0 +1,20 @@
+import torch
+from torch import nn
+
+class MLP(nn.Module):
+    def __init__(self, input_dim: int, output_dim: int=7):
+        super().__init__()
+
+        self._input_dim = input_dim
+        self._output_dim = output_dim
+
+        self.network = nn.Sequential(
+            nn.Linear(self._input_dim, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, self._output_dim),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.network(x)
