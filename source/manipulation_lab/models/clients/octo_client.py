a1b671f9d94d89d8f260fa120ae7097f5e4d906f
j9smith <j.smith9103@outlook.com>
2025-07-21 13:57:41 +0000

    Cleaned up obs handling and added obs history

--- source/manipulation_lab/models/clients/octo_client.py
+++ source/manipulation_lab/models/clients/octo_client.py
@@ -22,6 +22,9 @@ class OctoClient:
         self.socket.connect(address)
         self.device = device
 
+        self.step = 0
+        self._prev_obs = None
+
     def to(self, device: str):
         self.device = device
         return self
@@ -33,6 +36,7 @@ class OctoClient:
         pass
 
     def __call__(self, obs: tuple):
+        self.step += 1
         camera_obs, proprio_obs, _ = obs
         
         # Camera data actually comes in as nparray.
@@ -41,36 +45,39 @@ class OctoClient:
         wrist_camera = torch.Tensor(camera_obs["sensors/wrist_camera/rgb"])
         joint_pos = proprio_obs["robot/joint_pos"]
 
-
         wrist_camera = wrist_camera.permute(2, 0, 1).unsqueeze(0) # (H, W, C) -> (1, C, H, W)
 
         # Resize wristcam from 256x256 to 128x128
         wrist_camera = torch.nn.functional.interpolate(wrist_camera, size=(128, 128), mode="bilinear")
-        
         wrist_camera = wrist_camera.squeeze(0).permute(1, 2, 0) # (1, C, H, W) -> (H, W, C)
 
         def _process_camera(obs: torch.Tensor):
-            obs = obs.cpu()
-            obs = obs.unsqueeze(0).unsqueeze(0)
-            np_obs = obs.numpy()
-
+            obs = obs.cpu().unsqueeze(0).unsqueeze(0).numpy().astype(np.float32)
             # Per Octo paper, images are normalised to [-1, 1]
-            np_obs = np_obs.astype(np.float32) / 127.5 - 1.0
-            return np_obs
+            return obs / 127.5 - 1.0
 
-        octo_obs = {}
+        prim1 = _process_camera(prim_camera)
+        wrist1 = _process_camera(wrist_camera)
 
-        # All obs need leading (T, B, ...) dimensions
-        octo_obs["image_primary"] = _process_camera(prim_camera)
-        octo_obs["image_wrist"] = _process_camera(wrist_camera)
-        octo_obs["robot"] = joint_pos.reshape(1, 1, -1)
+        if self._prev_obs is None:
+            prim0, wrist0, step0 = prim1, wrist1, self.step
+        else:
+            prim0, wrist0, step0 = self._prev_obs
+            
+        octo_obs = {
+            "image_primary": np.concatenate([prim0, prim1], axis=1),
+            "image_wrist": np.concatenate([wrist0, wrist1], axis=1),
+            "timestep": np.array([[step0, self.step]]),
+            "timestep_pad_mask": np.array([[True, True]]),
+            "pad_mask_dict": {
+                "timestep": np.array([[True, True]]),
+                "image_primary": np.array([[True, True]]),
+                "image_wrist": np.array([[True, True]]),
+            },
+            "task_completed": np.zeros((1, 2, 4), dtype=bool),
+        }
 
-        # Add masking that Octo complains about
-        octo_obs["timestep_pad_mask"] = np.array([[True]])
-        octo_obs["pad_mask_dict/timestep"] = np.array([[True]])
-        octo_obs["pad_mask_dict/image_primary"] = np.array([[True]])
-        octo_obs["pad_mask_dict/image_wrist"] = np.array([[True]])
-
+        self._prev_obs = prim1, wrist1, self.step
         self.socket.send(pickle.dumps(octo_obs))
 
         msg = self.socket.recv()
