7577638db3e27e62a503d0d9499dbd8ead4ef3e2
j9smith <j.smith9103@outlook.com>
2025-07-28 10:51:36 +0000

    Refactored to use teleop only as an override when necessary, deferring to the policy otherwise.

--- source/manipulation_lab/scripts/teleop/teleop_handler.py
+++ source/manipulation_lab/scripts/teleop/teleop_handler.py
@@ -8,6 +8,7 @@ from manipulation_lab.scripts.control.obs_handler import ObservationHandler
 from manipulation_lab.scripts.dataset.writer import DatasetWriter
 from manipulation_lab.scripts.teleop.controller_interface import ControllerInterface
 import time
+import torch
 
 class TeleopHandler:
     def __init__(self, env, cfg):
@@ -23,11 +24,8 @@ class TeleopHandler:
         # TODO: If we're using the policy network to control the robot, we may
         # need to update ActionHandler's control_mode to reflect the action space
         if self.dagger == True:
-            import os
             from threading import Event
             from manipulation_lab.scripts.control.controller import Controller
-
-            cfg.dataset_writer.save_dir = os.path.join(cfg.dataset_writer.save_dir, "dagger")
 
             self.control_event = Event()
 
@@ -108,44 +106,66 @@ class TeleopHandler:
                     self._advance_phase()
 
             def _continue_episode():
+                recorded_actions = {}
+                use_policy_as_expert = False
+
                 obs = self.obs_handler.get_obs()
 
                 # Get the teleoperated action
                 teleop_action = self.teleop_controller.get_action()
 
-                recorded_actions = {
-                    "expert": {
-                        "ee_deltas": teleop_action[:6],
-                        "gripper_deltas": teleop_action[6]
-                    },
-                }
-
                 if self.dagger:
+                    # If no manual override (accounting for deadzone drift),
+                    # defer to the policy as expert
+                    if torch.all(torch.abs(teleop_action) < 5e-3):
+                        use_policy_as_expert = True
+
                     self.controller.sim_step_count = self.sim_steps
                     self.controller.update_obs(obs)
                     self.control_event.set()
                     controller_action = self.controller.get_action()
 
-                    recorded_actions["policy"] = {
-                        "ee_deltas": controller_action[:6],
-                        "gripper_deltas": controller_action[6:]
-                    }
+                    if controller_action is not None:
+                        recorded_actions["policy"] = {
+                            "ee_deltas": controller_action[:6].cpu(),
+                            "gripper_deltas": controller_action[6].cpu()
+                        }
+                    
+                    if use_policy_as_expert and controller_action is not None:
+                        recorded_actions["expert"] = {
+                            "ee_deltas": controller_action[:6].cpu(),
+                            "gripper_deltas": controller_action[6].cpu()
+                        }
+                    else:
+                        recorded_actions["expert"] = {
+                            "ee_deltas": teleop_action[:6].cpu(),
+                            "gripper_deltas": teleop_action[6].cpu()
+                        }
+                else:
+                    recorded_actions["expert"] = {
+                            "ee_deltas": teleop_action[:6].cpu(),
+                            "gripper_deltas": teleop_action[6].cpu()
+                        }
 
                 # Record observations at target FPS
                 if self.sim_steps % capture_frequency == 0:
+                    if use_policy_as_expert:
+                        logger.info("Recording policy as expert")
+                    else: logger.info("Recording teleop as expert")
                     is_first = (self.sim_steps == 0)
                     is_last = False # TODO: Add _get_dones to task scene, then put it here (returns bool)
                     self.dataset_writer.append_frame(
                         obs=obs,
-                        action=recorded_actions,
+                        actions=recorded_actions,
                         task_phase=self._current_phase,
                         is_first=is_first,
                         is_last=is_last,
                         sim_steps=self.sim_steps
                     )
 
-                if self.dagger and controller_action is not None:
-                    self.action_handler.apply(action=controller_action)
+                if self.dagger:
+                    if controller_action is not None:
+                        self.action_handler.apply(action=controller_action)
                 else:
                     self.action_handler.apply(action=teleop_action)
 
@@ -155,6 +175,9 @@ class TeleopHandler:
             self.scene.update(self.sim_dt)
         
     def _reset_scene(self):
+        if self.dagger:
+            self.controller.reset()
+
         # TODO: Do some ablations here to find out what we can get rid of
         logger.info("Resetting scene ...")
         self.env.reset()
