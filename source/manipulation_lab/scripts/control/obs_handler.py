66fa6ff137f921ce16b0210ef845424a6185f8c6
j9smith <j.smith9103@outlook.com>
2025-08-05 15:35:49 +0000

    Oracle observations are now calculated in the robot frame instead of world frame. Also added EE pose relative to robot as an obs.

--- source/manipulation_lab/scripts/control/obs_handler.py
+++ source/manipulation_lab/scripts/control/obs_handler.py
@@ -1,4 +1,7 @@
 """Handles observations from the environment."""
+from isaaclab.utils.math import subtract_frame_transforms
+import numpy as np
+import torch
 
 class ObservationHandler:
     def __init__(self, env):
@@ -7,8 +10,10 @@ class ObservationHandler:
         self.robot = self.scene.articulations["robot"]
         self.sensors = self.scene.sensors
 
+        self.robot_root_pose_w = self.robot.data.root_pose_w
+        self.robot_xyz_w, self.robot_quat_w = self.robot_root_pose_w[:, 0:3], self.robot_root_pose_w[:, 3:7]
+
     def get_obs(self):
-        self._get_oracle_obs()
         return {
             "sensors": self._get_sensor_obs(),
             "robot": self._get_robot_obs(),
@@ -34,9 +39,14 @@ class ObservationHandler:
         joint_pos = self.robot.data.joint_pos.squeeze(0).cpu().numpy()
         joint_vel = self.robot.data.joint_vel.squeeze(0).cpu().numpy()
 
+        ee_pose_w = self.robot.data.body_pose_w[:, -1]
+        ee_xyz_r, ee_quat_r = self._in_robot_frame(ee_pose_w)
+        ee_pose_r = np.concatenate((ee_xyz_r, ee_quat_r), axis=0)
+
         robot_obs = {
             "joint_pos": joint_pos,
-            "joint_vel": joint_vel
+            "joint_vel": joint_vel,
+            "ee_pose_r": ee_pose_r,
         }
         return robot_obs
     
@@ -44,13 +54,30 @@ class ObservationHandler:
         oracle_obs = {}
 
         for object_name, obj in self.scene.rigid_objects.items():
-            pose = obj.data.root_link_pose_w.squeeze(0).cpu().numpy()
-            vel = obj.data.root_link_vel_w.squeeze(0).cpu().numpy()
+            obj_xyz_r, obj_quat_r = self._in_robot_frame(obj.data.root_link_pose_w)
+            pose_r = np.concatenate((obj_xyz_r, obj_quat_r), axis=0)
+            pose_w = obj.data.root_link_pose_w.squeeze(0).cpu().numpy()
+            vel_w = obj.data.root_link_vel_w.squeeze(0).cpu().numpy()
 
             oracle_obs[object_name] = {
-                "pose": pose,
-                "velocity": vel,
+                "pose_w": pose_w,
+                "velocity_w": vel_w,
+                "pose_r": pose_r,
             }
 
         return oracle_obs
+    
+    def _in_robot_frame(self, obj_root_pose_w: torch.Tensor):
+        """
+        Calculates the position of a given object in the robot's frame.
+        """
+        obj_xyz_w, obj_quat_w = obj_root_pose_w[:, 0:3], obj_root_pose_w[:, 3:7]
 
+        obj_xyz_r, obj_quat_r = subtract_frame_transforms(
+            t01=self.robot_xyz_w,
+            q01=self.robot_quat_w,
+            t02=obj_xyz_w,
+            q02=obj_quat_w
+        )
+        
+        return obj_xyz_r[0].cpu().numpy(), obj_quat_r[0].cpu().numpy()
